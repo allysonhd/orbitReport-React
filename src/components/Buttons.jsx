60e71382b175f2374e9ca8c31e25ecb5d2cf87ae
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-09-02 11:58:44 -0500

    Buttons file complete

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,18 @@
+import satData from "./satData";
+// import Styling from "./components/styling";
+// className="flex-container"
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}> All Orbits</button>
     </div>
   );
 };
