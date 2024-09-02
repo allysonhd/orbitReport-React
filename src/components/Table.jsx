a2e0cf3ea9293980b3f2bc2a5619a37e6043efbf
Your Name <166686180+allysonhd@users.noreply.github.com>
2024-09-02 12:00:55 -0500

    Table complete

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,27 @@
+// import Styling from "./components/styling";
+
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((data) => (
+          <tr key={data.id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational == true ? "active" : "inactive"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
-
 export default Table;
